570834688b04c07440c2bb66554c457647abe6b3
Parkjju <rudwns3927@gmail.com>
2023-05-15 22:09:56 +0900

    Initial Commit

---
+++ pullup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pullup
+//
+//  Created by Jun on 2023/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
